e48717f82d02aa0692a8754183050a66df15c905
Võ Hoàng Gia Bảo <snowline2015@users.noreply.github.com>
2020-11-20 11:41:50 +0700

    ok

--- mmt_19clc9/Client/Source.cpp
+++ mmt_19clc9/Client/Source.cpp
@@ -1,43 +1,107 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
-#include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <string>
+#include <thread>
+#include <stdexcept>
+#include <sstream>
+#include <iomanip>
+#include <cstdint>
 #pragma comment( lib, "wsock32.lib" )
-#define PORT 5000
+#define PORT "5000"
+
+std::string string_to_hex(const std::string& in) {
+    std::stringstream ss;
+
+    ss << std::hex << std::setfill('0');
+    for (size_t i = 0; in.length() > i; ++i) {
+        ss << std::setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(in[i]));
+    }
+
+    return ss.str();
+}
 
 int main() {
-	std::string str;
-	std::cout << "Input client ip address: ";
-	getline(std::cin, str);
-	const char* c = str.c_str();
+    struct sockaddr_in addrport;
+    struct sockaddr_in* server = NULL, * ptr = NULL, * result = NULL;
+    SOCKET sockid = INVALID_SOCKET, NewSockid;
+    std::string msg = "";
+    int iResult = 0;
 
-	WSADATA wsaData;
-	int wsOK = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (wsOK != 0) {
-		std::cout << "Can't initialize winsock. Application is now exiting..." << std::endl;
-		return 0;
-	}
+    std::string str;
+    std::cout << "Input client ip address: ";
+    getline(std::cin, str);
+    const char* c = str.c_str();
 
-	SOCKET sockid = socket(PF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in addrport;
-	addrport.sin_family = AF_INET;
-	addrport.sin_port = htons(PORT);
-	addrport.sin_addr.s_addr = inet_addr(c); //htonl(INADDR_ANY);// 
-	connect(sockid, (struct sockaddr*)&addrport, sizeof(addrport));
-	while (true) {
-		char* temp = new char[200];
-		if (recv(sockid, temp, sizeof(temp), 0) == 0) {
-			temp[strlen(temp) - 1] = '\0';
-			std::cout << "Server: " << temp << std::endl;
-		}
-		/*else {
-			std::cout << "Message: ";
-			getline(std::cin, str);
-			const char* c = str.c_str();
-			send(sockid, c, sizeof(c), 0);*/
-		//}
-	}
-	WSACleanup();
-	return 0;
+    std::cout << "Intializing Winsock..." << std::endl;
+
+    WSADATA wsaData;
+    int wsOK = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsOK != 0) {
+        std::cout << "Can't initialize winsock. Application is now exiting..." << std::endl;
+        return 0;
+    }
+
+    ZeroMemory(&addrport, sizeof(addrport));
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(5000);
+    addrport.sin_addr.s_addr = inet_addr(c);
+
+    /*std::cout << "Setting up server..." << std::endl;
+    getaddrinfo(IP_ADDRESS, PORT, &hints, &server);*/
+
+    sockid = socket(PF_INET, SOCK_STREAM, 0);
+
+    // Attempt to connect to an address until one succeeds
+    while (true) {
+        // Connect to server.
+        iResult = connect(sockid, (struct sockaddr*)&addrport, sizeof(addrport));
+        NewSockid = connect(sockid, (struct sockaddr*)&addrport, sizeof(addrport));
+        if (iResult == SOCKET_ERROR) {
+            closesocket(NewSockid);
+            NewSockid = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    if (NewSockid == INVALID_SOCKET) {
+        std::cout << "Unable to connect to server!" << std::endl;
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    std::cout << "Successfully Connected" << std::endl;
+
+    char* temp = new char[200];
+    recv(NewSockid, temp, sizeof(temp), 0);
+
+    while (1)
+    {
+        getline(std::cin, str);
+        str = string_to_hex(str);
+        iResult = send(NewSockid, str.c_str(), strlen(str.c_str()), 0);
+
+        if (iResult <= 0)
+        {
+            std::cout << "send() failed: " << WSAGetLastError() << std::endl;
+            break;
+        }
+    }
+    std::cout << "Shutting down socket..." << std::endl;
+    iResult = shutdown(NewSockid, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        std::cout << "shutdown() failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(NewSockid);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    closesocket(NewSockid);
+    WSACleanup();
+    system("pause");
+    return 0;
 }
--- mmt_19clc9/Server/Source.cpp
+++ mmt_19clc9/Server/Source.cpp
@@ -1,57 +1,131 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
-#include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <string>
+#include <thread>
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+#include <iomanip>
+#include <cstdint>
 #pragma comment( lib, "wsock32.lib" )
-#define PORT 5000
+#define PORT "5000"
+const char OPTION_VALUE = 1;
+const int MAX_CLIENTS = 5;
 
-int main() {
-	std::string str;
-	std::cout << "Input client ip address: ";
-	getline(std::cin, str);
-	const char* c = str.c_str();
+struct client_type
+{
+    int id;
+    SOCKET socket;
+};
 
-	WSADATA wsaData;
-	int wsOK = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (wsOK != 0) {
-		std::cout << "Can't initialize winsock. Application is now exiting..." << std::endl;
-		return 0;
-	}
+int main()
+{
+    struct sockaddr_in addrport;
+    struct sockaddr_in* server = NULL;
+    SOCKET sockid = INVALID_SOCKET;
+    std::string msg = "";
+    std::vector<client_type> client(MAX_CLIENTS);
+    int num_clients = 0;
+    int temp_id = -1;
+    std::thread my_thread[MAX_CLIENTS];
 
-	SOCKET sockid = socket(PF_INET, SOCK_STREAM, 0), NewSockid;
-	struct sockaddr_in addrport;
-	int AddrSize = sizeof(addrport);
-	addrport.sin_family = AF_INET;
-	addrport.sin_port = htons(PORT);
-	//inet_pton(AF_INET, "192.0.2.33", &(addrport.sin_addr));
-	addrport.sin_addr.s_addr = inet_addr(c); //htonl(INADDR_ANY);
-	bind(sockid, (struct sockaddr*)&addrport, sizeof(addrport));
-	listen(sockid, SOMAXCONN);
-	while (true) {
-		NewSockid = accept(sockid, (struct sockaddr*)&addrport, &AddrSize);
-		if (NewSockid == INVALID_SOCKET) continue;
-		char* temp = new char[200];
-		/*if (recv(NewSockid, temp, sizeof(temp), 0) == 0) {
-			temp[strlen(temp) - 1] = '\0';
-			std::cout << "Client: " << temp << std::endl;
-			break;
-		}*/
-		//else {
-			std::cout << "Message: ";
-			getline(std::cin, str);
-			c = str.c_str();
-			send(NewSockid, c, sizeof(c), 0);
-		//}
-		closesocket(sockid);
-	}
+    std::string str;
+    std::cout << "Input client ip address: ";
+    getline(std::cin, str);
+    const char* c = str.c_str();
 
-		
-		/*else {
-			std::cout << "fail to connect to client !" << std::endl;
-			closesocket(sockid);
-		}*/
-	WSACleanup();
-	system("PAUSE");
-	return 0;
+    std::cout << "Intializing Winsock..." << std::endl;
+
+    WSADATA wsaData;
+    int wsOK = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsOK != 0) {
+        std::cout << "Can't initialize winsock. Application is now exiting..." << std::endl;
+        return 0;
+    }
+
+    ZeroMemory(&addrport, sizeof(addrport));
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(5000);
+    addrport.sin_addr.s_addr = inet_addr(c);
+
+    /*std::cout << "Setting up server..." << std::endl;
+    getaddrinfo(IP_ADDRESS, PORT, &hints, &server);*/
+
+    sockid = sockid = socket(PF_INET, SOCK_STREAM, 0);
+
+    ////Setup socket options
+    //setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &OPTION_VALUE, sizeof(int)); //Make it possible to re-bind to a port that was used within the last 2 minutes
+    //setsockopt(server_socket, IPPROTO_TCP, TCP_NODELAY, &OPTION_VALUE, sizeof(int)); //Used for interactive programs
+
+
+    bind(sockid, (struct sockaddr*)&addrport, sizeof(addrport));
+    listen(sockid, SOMAXCONN);
+
+    //Initialize the client list
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        client[i] = { -1, INVALID_SOCKET };
+    }
+
+    while (true)
+    {
+        SOCKET NewSockid = INVALID_SOCKET;
+        NewSockid = accept(sockid, NULL, NULL);
+
+        if (NewSockid == INVALID_SOCKET) continue;
+
+        //Reset the number of clients
+        num_clients = -1;
+
+        //Create a temporary id for the next client
+        temp_id = -1;
+        for (int i = 0; i < MAX_CLIENTS; i++)
+        {
+            if (client[i].socket == INVALID_SOCKET && temp_id == -1)
+            {
+                client[i].socket = NewSockid;
+                client[i].id = i;
+                temp_id = i;
+            }
+
+            if (client[i].socket != INVALID_SOCKET)
+                num_clients++;
+
+            //std::cout << client[i].socket << std::endl;
+        }
+
+        if (temp_id != -1)
+        {
+            //Send the id to that client
+            std::cout << "Client #" << client[temp_id].id << " Accepted" << std::endl;
+            msg = std::to_string(client[temp_id].id);
+            send(client[temp_id].socket, msg.c_str(), strlen(msg.c_str()), 0);
+
+            //Create a thread process for that client
+            //my_thread[temp_id] = std::thread(process_client, std::ref(client[temp_id]), std::ref(client), std::ref(my_thread[temp_id]));
+        }
+        else
+        {
+            msg = "Server is full";
+            send(NewSockid, msg.c_str(), strlen(msg.c_str()), 0);
+            std::cout << msg << std::endl;
+        }
+    } 
+
+
+    closesocket(sockid);
+
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        my_thread[i].detach();
+        closesocket(client[i].socket);
+    }
+
+    WSACleanup();
+    std::cout << "Program has ended successfully" << std::endl;
+
+    system("pause");
+    return 0;
 }

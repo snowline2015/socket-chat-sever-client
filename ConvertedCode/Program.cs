1eab08e578b0ad7e120b18905e5906e112759416
HuyHoang19127406 <63275057+HuyHoang19127406@users.noreply.github.com>
2020-12-10 00:21:51 +0700

    cho cho c# code

---
+++ ConvertedCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvertedCode
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Co gi cu code het vao day xong khi nao xong cai ui se dua vao MainWindow.xaml.cs
+        }
+    }
+}

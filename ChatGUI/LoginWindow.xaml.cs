22888f14c20bff78cac2826b7fbc10f2ad37dbad
HuyHoang19127406 <63275057+HuyHoang19127406@users.noreply.github.com>
2020-12-18 14:54:23 +0700

    finish register

--- ChatGUI/LoginWindow.xaml.cs
+++ ChatGUI/LoginWindow.xaml.cs
@@ -114,5 +114,57 @@ namespace ChatGUI
                 MessageBox.Show(ex.Message, "Error occurred", MessageBoxButton.OK);
             }
         }
+
+        private void EnterRegister(object sender, RoutedEventArgs e)
+        {
+            if (LoginGrid.Visibility == Visibility.Visible)
+                LoginGrid.Visibility = Visibility.Collapsed;
+            RegisterGrid.Visibility = Visibility.Visible;
+        }
+
+        private void ReturnLogin(object sender, RoutedEventArgs e)
+        {
+            if (RegisterGrid.Visibility == Visibility.Visible)
+                RegisterGrid.Visibility = Visibility.Collapsed;
+            LoginGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            if(Name.Text.Length == 0 || UsrName_reg.Text.Length == 0 || Pass_reg.Text.Length == 0 || Mail_reg.Text.Length == 0)
+            {
+                MessageBox.Show("Please fill all information");
+                return;
+            }
+            try
+            {
+                SqlConnection connection = default(SqlConnection);
+                connection = new SqlConnection(db);
+                string cmd = "Insert into Users (Username,Password,Name,Email,Birthday) VALUES (@Username,@Password,@Name,@Email,@Birthday)";
+
+                SqlCommand command = default(SqlCommand);
+                command = new SqlCommand(cmd, connection);
+
+                connection.Open();
+                command.Parameters.AddWithValue("@Username", UsrName_reg.Text);
+                command.Parameters.AddWithValue("@Password", Pass_reg.Text);
+                command.Parameters.AddWithValue("@Name", Name.Text);
+                command.Parameters.AddWithValue("@Email", Mail_reg.Text);
+                command.Parameters.AddWithValue("@Birthday", Birthday.SelectedDate.Value.Date.ToShortDateString());
+
+                command.ExecuteNonQuery();
+                MessageBox.Show("Success");
+                connection.Close();
+
+                if(connection.State == ConnectionState.Open)
+                {
+                    connection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error occurred", MessageBoxButton.OK);
+            }
+        }
     }
 }
